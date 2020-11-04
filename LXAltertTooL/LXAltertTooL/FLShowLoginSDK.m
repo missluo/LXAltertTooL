e99fa3033dd154bba7ad4933734a2964a3594423
binbin <18301511564@163.com>
2020-11-04 17:28:08 +0800

    push  init

---
+++ LXAltertTooL/LXAltertTooL/FLShowLoginSDK.m
@@ -0,0 +1,69 @@
+//
+//  FLShowLoginSDK.m
+//  FLShowLoginSDK
+//
+//  Created by binbin on 2020/11/4.
+//
+
+#import "FLShowLoginSDK.h"
+#import <UIKit/UIKit.h>
+@implementation FLShowLoginSDK
+
+
++ (void)showAlerView {
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"提示" message:@"message" preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        NSLog(@"取消哈哈");
+    }];
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        NSLog(@"确定");
+    }];
+    
+    [alertController addAction:cancelAction];
+    [alertController addAction:okAction];
+    [[self getCurrentVC] presentViewController:alertController animated:YES completion:nil];
+}
+
++ (UIViewController *)getCurrentVC {
+    UIViewController *result = nil;
+    
+    UIWindow * window = [[UIApplication sharedApplication] keyWindow];
+    
+    if (window.windowLevel != UIWindowLevelNormal)
+    {
+        NSArray *windows = [[UIApplication sharedApplication] windows];
+        for(UIWindow * tmpWin in windows)
+        {
+            if (tmpWin.windowLevel == UIWindowLevelNormal)
+            {
+                window = tmpWin;
+                break;
+            }
+        }
+    }
+    if (window.subviews.count > 0) {
+        UIView *frontView = [[window subviews] objectAtIndex:0];
+        id nextResponder = [frontView nextResponder];
+        
+        if ([nextResponder isKindOfClass:[UIViewController class]])
+            result = nextResponder;
+        else
+            result = window.rootViewController;
+        
+        return result;
+    }
+    return nil;
+}
+
++ (UIViewController *)_topViewController:(UIViewController *)vc {
+    if ([vc isKindOfClass:[UINavigationController class]]) {
+        return [self _topViewController:[(UINavigationController *)vc topViewController]];
+    } else if ([vc isKindOfClass:[UITabBarController class]]) {
+        return [self _topViewController:[(UITabBarController *)vc selectedViewController]];
+    } else {
+        return vc;
+    }
+    return nil;
+}
+@end
