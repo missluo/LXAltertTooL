910684dfe3bca29759998f8cf6e92c6dda861089
binbin <18301511564@163.com>
2020-11-04 18:05:10 +0800

    push podspec

---
+++ Classes/LXAltertTooL.framework/Headers/FLShowLoginSDK.h
@@ -0,0 +1,13 @@
+//
+//  FLShowLoginSDK.h
+//  FLShowLoginSDK
+//
+//  Created by binbin on 2020/11/4.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FLShowLoginSDK : NSObject
+
++ (void)showAlerView;
+@end
---
+++ Classes/LXAltertTooL.framework/Headers/LXAltertTooL.h
@@ -0,0 +1,19 @@
+//
+//  LXAltertTooL.h
+//  LXAltertTooL
+//
+//  Created by binbin on 2020/11/4.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LXAltertTooL.
+FOUNDATION_EXPORT double LXAltertTooLVersionNumber;
+
+//! Project version string for LXAltertTooL.
+FOUNDATION_EXPORT const unsigned char LXAltertTooLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LXAltertTooL/PublicHeader.h>
+
+
+#import "FLShowLoginSDK.h"
